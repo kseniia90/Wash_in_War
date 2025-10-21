cda345269faddd15e5af37ad44f7c866fcc68684
Dmytro Pukhoviy <poohdima@gmail.com>
2025-10-21 23:00:19 +0300

    Front page multilang

--- wp-content/themes/washinwar/front-page.php
+++ wp-content/themes/washinwar/front-page.php
@@ -1,26 +1,10 @@
-<?php get_header(); ?>
-
-<?php /*
-
-<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
-    <header class="page-header alignwide">
-        <h1 class="page-title"><?php single_post_title(); ?></h1>
-    </header>
-<?php endif; ?>
-
 <?php
-if ( have_posts() ) {
-    while ( have_posts() ) {
-        the_post();
-//        get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
-    }
+get_header();
+if (get_locale() === 'en_US') {
+	get_template_part('front-page-en');
+} elseif (get_locale() === 'nn_NO') {
+	get_template_part('front-page-no');
 } else {
-    // If no content, include the "No posts found" template.
-    get_template_part( 'template-parts/content/content-none' );
+	get_template_part('front-page-ua');
 }
-*/ ?>
-
-
-	<?php get_template_part( 'front-page-ua' ); ?>
-
-<?php get_footer();
+get_footer();
